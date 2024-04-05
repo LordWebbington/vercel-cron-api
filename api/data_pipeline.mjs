487348424fac63e46c167ce4d43b46edf89a4735
Chris Webster <chris@websterpropertygroup.com>
2024-04-05 16:58:25 -0700

    Convert to ES module syntax

--- api/data_pipeline.mjs
+++ api/data_pipeline.mjs
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
 
   const SUPABASE_URL = process.env.SUPABASE_URL;
   const SUPABASE_KEY = process.env.SUPABASE_KEY;
